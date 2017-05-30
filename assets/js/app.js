ece9196d6e359d954c95b5d87fafa77e8c135c3c
Gaby Trejo <gabytrejo303@gmail.com>
2017-05-30 12:38:04 -0500

    carrusel con JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,43 @@
+var target = 0;
 
-$(document).ready(function(){
-	console.log("Hola! jQuery");
-});
+var cargarPagina = function () {
+    // Elementos
+    var botones = document.querySelectorAll(".control");
+    var anterior = document.querySelector(".previo");
+    var siguiente = document.querySelector(".sig");
 
-$(document).ready(function(){
-	
-    $("#dot0").click(function(){
-        $("img").hide();
-        $("#img0").show();
+    //Eventos
+    botones.forEach(function(boton){
+        boton.addEventListener("click", cambioDeImagen);
     });
-    $("#dot1").click(function(){
-        $("img").hide();
-        $("#img1").show();
-    });
-    $("#dot2").click(function(){
-        $("img").hide();
-        $("#img2").show();
-    });
-    $("#dot3").click(function(){
-        $("img").hide();
-        $("#img3").show();
-    });
-    $("#dot4").click(function(){
-        $("img").hide();
-        $("#img4").show();
-    });
-});
+    anterior.addEventListener("click", imgAnterior);
+    siguiente.addEventListener("click", imgSiguiente);
+};
+
+var cambioDeImagen = function () {
+    var target = parseInt(this.dataset.target);
+    mostrarImagen(target);    
+};
+
+var mostrarImagen = function (target) {
+    var ultimaSlide = document.querySelector("div.activo");
+    var slide = document.querySelector("div[data-slide='" + target + "']");
+    ultimaSlide.classList.remove("activo");
+    slide.classList.add("activo");
+};
+
+var imgAnterior = function (e) {
+    e.preventDefault();
+    target = target - 1;
+    target = (target < 0) ? 3 : target;
+    mostrarImagen(target);
+};
+
+var imgSiguiente = function (e) {
+    e.preventDefault();
+    target = target + 1;
+    target = (target > 3) ? 0 : target;
+    mostrarImagen(target);mostrarImagen(target);
+};
+
+window.addEventListener("load", cargarPagina);
